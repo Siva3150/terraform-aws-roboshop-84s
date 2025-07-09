9472aee4053a19339672d8cf8727083aafce1e91
Siva prasad <sivaprasad3150@gmail.com>
2025-07-09 11:42:52 +0530

    user-profile

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z02186033QPSEMV5IIWBW"
+}
+
+variable "zone_name" {
+    default = "sivadevops.fun"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
